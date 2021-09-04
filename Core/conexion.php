7fef0d963dfad044e704ecf79db4a84850f1ff0d
scalarcon <camilo-lara01@hotmail.com>
2021-09-04 12:23:27 -0500

    Conexión

---
+++ Core/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$cnn = Conexion::getConexion();
+
+class Conexion
+{
+    public static function getConexion()
+    {
+        $cnn = null;
+        try {
+            $cnn = new PDO("mysql:host=gestioncmc.ddns.net;dbname=ventaproducto", "root", "newral05");
+            $cnn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            echo "Error $ex->getMessage();";
+        }
+        return $cnn;
+    }
+}
+?>
